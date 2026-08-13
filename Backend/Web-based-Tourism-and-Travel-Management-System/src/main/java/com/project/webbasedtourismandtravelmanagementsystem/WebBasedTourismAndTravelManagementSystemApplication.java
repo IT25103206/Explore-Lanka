686ec867a4d156d9f57454c0759fd2c674e05812
Ometh Kodagoda <it25103206@my.sliit.lk>
2026-08-13 11:50:18 +0530

    1.0 - Initialization Commit
    Initialization Commit of the Java Based Web Project - Web Based Tourism and Travel Management System.

---
+++ Backend/Web-based-Tourism-and-Travel-Management-System/src/main/java/com/project/webbasedtourismandtravelmanagementsystem/WebBasedTourismAndTravelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.webbasedtourismandtravelmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedTourismAndTravelManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedTourismAndTravelManagementSystemApplication.class, args);
+    }
+
+}
---
+++ Backend/Web-based-Tourism-and-Travel-Management-System/src/test/java/com/project/webbasedtourismandtravelmanagementsystem/WebBasedTourismAndTravelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webbasedtourismandtravelmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedTourismAndTravelManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
